aeabb2e333a702137f1ab170c03fdb03f516da83
kyoowon <kyoowon95@gmail.com>
2022-03-30 14:21:12 +0900

    Auth project - add login functions (jwt& cookie)

--- Auth_jwt/src/index.ts
+++ Auth_jwt/src/index.ts
@@ -1,23 +1,24 @@
 import express, {Request, Response} from 'express';
 import bodyParser from "body-parser";
-import User from './module/User';
+import User from './models/User';
 
-const app = express()
+const app = express(); // express.js
+const mongoose = require('mongoose'); // mongodb Connect
+const config = require('./config/key'); // mongoDB URI key
+const cookieParser = require('cookie-parser'); // cookie
+
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
-})
-
+// port 
 const port = 4000
-const mongoose = require('mongoose') // mongodb Connect
-const config = require('./config/key');
 
+// mondoDB connection
 mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB connected')).catch((err: Error) => console.log(err))
 
@@ -44,7 +45,13 @@ app.post('/api/users/login', (req: Request, res: Response) => {
         message: "작성한 이메일에 해당하는 유저가 없습니다."
       })
     }
-    
+    // 비밀번호까지 같다면 토큰을 생성
+    user.generateToken((err: Error, user: typeof User) => {
+      if (err) return res.status(400).send(err);
+
+      // 토큰을 저장한다. 어디에? - 캐쉬, 로컬스토리지, 세션, 쿠키 등 저장을 할 수 있다. - 그중 쿠키로 저장
+      res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id })
+    })
   })
 })
 
